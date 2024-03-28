28d8cf09c6f84e556b6634219e372da7492896d0
rapha <la228038@student.helha.be>
2024-03-28 11:58:58 +0100

    simplify request

--- src/main/java/com/example/applicine/database/ApiRequest.java
+++ src/main/java/com/example/applicine/database/ApiRequest.java
@@ -10,102 +10,73 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.sql.SQLException;
 
-
-
 public class ApiRequest {
 
     private static final String APIkey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTlkY2U5OGE2MmRiZjY1MTVjMzIwNTNiNmIwNDRlZCIsInN1YiI6IjY2MDE2YTZmMzc4MDYyMDE2MjNhMWQxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tmTHxA8Y_vY4aNKMW26hL2pffx4jFX-RZZThVSYX-j0";
+
     public static Response getMovies() throws IOException {
+        return executeRequest("https://api.themoviedb.org/3/movie/now_playing?language=fr-BE&page=1");
+    }
+
+    public void addMoviesToDatabase() throws IOException, SQLException {
+        JSONArray results = new JSONObject(getMovies().body().string()).getJSONArray("results");
+
+        for (int i = 0; i < results.length(); i++) {
+            JSONObject movieJson = results.getJSONObject(i);
+            Movie movie = createMovieFromJson(movieJson);
+            DatabaseConnection.AddMovie(movie);
+        }
+    }
+
+    public Response getMovieDetails(int movieId) throws IOException {
+        return executeRequest("https://api.themoviedb.org/3/movie/" + movieId + "?language=en-US");
+    }
+
+    public Response getMovieCredits(int movieId) throws IOException {
+        return executeRequest("https://api.themoviedb.org/3/movie/" + movieId + "/credits?language=en-US");
+    }
+
+    private static Response executeRequest(String url) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url("https://api.themoviedb.org/3/movie/now_playing?language=fr-BE&page=1")
+                .url(url)
                 .get()
                 .addHeader("accept", "application/json")
                 .addHeader("Authorization", APIkey)
                 .build();
 
-        Response response = client.newCall(request).execute();
-        return response;
+        return client.newCall(request).execute();
     }
 
-    public void addMoviesToDatabase() throws IOException, SQLException {
-    Response response = getMovies();
-    String jsonData = response.body().string();
-    JSONObject jobj = new JSONObject(jsonData);
-    JSONArray jarr = jobj.getJSONArray("results");
+    private Movie createMovieFromJson(JSONObject movieJson) throws IOException, SQLException {
+        int movieId = movieJson.getInt("id");
+        JSONObject detailsObj = new JSONObject(getMovieDetails(movieId).body().string());
+        JSONObject creditsObj = new JSONObject(getMovieCredits(movieId).body().string());
 
-    for (int i = 0; i < jarr.length(); i++) {
-        JSONObject movieJson = jarr.getJSONObject(i);
         String title = movieJson.getString("title");
         String synopsis = movieJson.getString("overview");
-        String imagePath = movieJson.getString("poster_path");
-        String imageUrl = "https://image.tmdb.org/t/p/w500" + imagePath;
-
-        int movieId = movieJson.getInt("id");
-
-        // Get movie details
-        Response detailsResponse = getMovieDetails(movieId);
-        String detailsData = detailsResponse.body().string();
-        JSONObject detailsObj = new JSONObject(detailsData);
-        JSONArray genresArr = detailsObj.getJSONArray("genres");
-        String genre = genresArr.getJSONObject(0).getString("name"); // Get the first genre
+        String imageUrl = "https://image.tmdb.org/t/p/w500" + movieJson.getString("poster_path");
+        String genre = detailsObj.getJSONArray("genres").getJSONObject(0).getString("name");
         int duration = detailsObj.getInt("runtime");
+        String director = getDirectorFromCredits(creditsObj);
 
-        // Get movie credits
-        Response creditsResponse = getMovieCredits(movieId);
-        String creditsData = creditsResponse.body().string();
-        JSONObject creditsObj = new JSONObject(creditsData);
+        return new Movie(title, genre, director, duration, synopsis, imageUrl);
+    }
+
+    private String getDirectorFromCredits(JSONObject creditsObj) {
         JSONArray crewArr = creditsObj.getJSONArray("crew");
-        String director = "";
         for (int j = 0; j < crewArr.length(); j++) {
             JSONObject crewMember = crewArr.getJSONObject(j);
             if (crewMember.getString("job").equals("Director")) {
-                director = crewMember.getString("name");
-                break;
+                return crewMember.getString("name");
             }
         }
-
-        Movie movie = new Movie(title, genre, director, duration, synopsis, imageUrl);
-        try {
-            DatabaseConnection.AddMovie(movie);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        return "";
     }
-}
-
-public Response getMovieDetails(int movieId) throws IOException {
-    OkHttpClient client = new OkHttpClient();
-
-    Request request = new Request.Builder()
-            .url("https://api.themoviedb.org/3/movie/" + movieId + "?language=en-US")
-            .get()
-            .addHeader("accept", "application/json")
-            .addHeader("Authorization", APIkey)
-            .build();
-
-    Response response = client.newCall(request).execute();
-    return response;
-}
-
-public Response getMovieCredits(int movieId) throws IOException {
-    OkHttpClient client = new OkHttpClient();
-
-    Request request = new Request.Builder()
-            .url("https://api.themoviedb.org/3/movie/" + movieId + "/credits?language=en-US")
-            .get()
-            .addHeader("accept", "application/json")
-            .addHeader("Authorization", APIkey)
-            .build();
-
-    Response response = client.newCall(request).execute();
-    return response;
-}
 
     public static void main(String[] args) throws IOException, SQLException {
         DatabaseConnection.deleteTableEntries();
-        ApiRequest apiRequest = new ApiRequest();
-        apiRequest.addMoviesToDatabase();
+        new ApiRequest().addMoviesToDatabase();
     }
 }
