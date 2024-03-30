89f221a9610079c9ff4d63615d2090e4c971b6bf
Raphaeldufour <la228038@student.helha.be>
2024-03-30 12:43:21 +0100

    dao database connection fixed

--- src/main/java/com/example/applicine/database/DatabaseConnection.java
+++ src/main/java/com/example/applicine/database/DatabaseConnection.java
@@ -8,13 +8,13 @@ public class DatabaseConnection {
     private static Connection connection;
     private static final String DbURL = "jdbc:sqlite:src/main/resources/com/example/applicine/views/database/CinemaTor.db";
     public static Connection getConnection() {
-        if (connection == null) {
-            try {
+        try {
+            if (connection == null || connection.isClosed()) {
                 connection = DriverManager.getConnection(DbURL);
                 System.out.println("Connexion à la base de données établie");
-            } catch (SQLException e) {
-                System.out.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
             }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
         }
         return connection;
     }
