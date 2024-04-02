e307d9b0f332ff2a2e43e8a23a498f1dc039169a
Dylan <la228628@student.helha.be>
2024-04-02 21:17:24 +0200

    AddButton and ModifyButton work with the masterBranch

---
+++ src/main/java/com/example/applicine/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.applicine;
+
+import com.example.applicine.controllers.MasterApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MasterApplication.main(args);
+    }
+}
