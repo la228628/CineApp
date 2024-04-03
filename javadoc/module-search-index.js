64e590768e9d7d452a563bb10b9efc9bed3b8b59
Raphaeldufour <la228038@student.helha.be>
2024-04-03 23:25:06 +0200

    javadoc

---
+++ javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.example.applicine"}];updateSearchResults();
---
+++ src/test/java/module-info.java
@@ -0,0 +1,7 @@
+module com.example.applicine.test {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.junit.jupiter.api;
+}
