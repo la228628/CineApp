f0b28b02a08060febeb22aea95042123b468edac
Raphaeldufour <la228038@student.helha.be>
2024-04-03 17:18:02 +0200

    javadoc de LoginControllerView et de MoviePaneController

--- src/main/java/com/example/applicine/views/MoviePaneController.java
+++ src/main/java/com/example/applicine/views/MoviePaneController.java
@@ -8,19 +8,39 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+/**
+ * Controller for the movie pane.
+ */
 public class MoviePaneController {
+    /**
+     * The root of the movie pane.
+     */
     @FXML
     private VBox root;
+    /**
+     * The image view of the movie pane.
+     */
     @FXML
     private ImageView imageView;
+    /**
+     * The title label of the movie pane.
+     */
     @FXML
     public Label titleLabel;
 
+    /**
+     * Sets the movie of the movie pane.
+     * @param movie the movie to set.
+     */
     public void setMovie(Movie movie) {
         titleLabel.setText(movie.getTitle());
         imageView.setImage(new Image(movie.getImagePath()));
     }
 
+    /**
+     * Gets the root of the movie pane.
+     * @return the root of the movie pane.
+     */
     public Pane getRoot() {
         return root;
     }
