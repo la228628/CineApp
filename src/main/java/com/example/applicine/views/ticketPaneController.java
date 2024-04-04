c72ba2bc05db9d89ac1591d5d4092e1ed146cfd1
Raphaeldufour <la228038@student.helha.be>
2024-04-04 16:00:07 +0200

    ticketPane.fxml et ticketPaneController

---
+++ src/main/java/com/example/applicine/views/ticketPaneController.java
@@ -0,0 +1,31 @@
+package be.helha.applicine.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ticketPaneController {
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    private ImageView imageMovieView;
+
+    @FXML
+    private Label roomLabel;
+
+    @FXML
+    private Label ticketTypeLabel;
+
+    @FXML
+    private Label timeLabel;
+
+    @FXML
+    private Text titleMovieLabel;
+
+    @FXML
+    private Label versionMovieLabel;
+
+}
