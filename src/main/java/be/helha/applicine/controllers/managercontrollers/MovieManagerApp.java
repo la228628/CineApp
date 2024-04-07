6241524ae7c4e977317c58742a14637712a2fc7a
Dylan <la228628@student.helha.be>
2024-04-07 21:54:18 +0200

    All work with the Roaming Folder

---
+++ src/main/java/be/helha/applicine/controllers/managercontrollers/MovieManagerApp.java
@@ -0,0 +1,225 @@
+package be.helha.applicine.controllers.managercontrollers;
+
+import be.helha.applicine.FileMangement.FileManager;
+import be.helha.applicine.models.Movie;
+import be.helha.applicine.models.exceptions.InvalideFieldsExceptions;
+import be.helha.applicine.views.managerviews.MovieManagerViewController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MovieManagerApp extends ManagerController implements MovieManagerViewController.ManagerViewListener{
+
+    private ManagerController parentController;
+
+    private FXMLLoader movieManagerFxmlLoader;
+
+    private MovieManagerViewController movieManagerViewController;
+
+    public MovieManagerApp() {
+        super();
+    }
+
+    @Override
+    public void start(Stage adminPage) throws Exception {
+        movieManagerFxmlLoader = parentController.getMovieManagerFXML();
+        movieManagerViewController = movieManagerFxmlLoader.getController();
+        movieManagerViewController.setListener(this);
+        for (Movie movie : movieList) {
+            movieManagerViewController.displayMovie(movie);
+            System.out.println(movie.getId());
+        }
+    }
+
+
+    public void setParentController(ManagerController parentController) {
+        this.parentController = parentController;
+    }
+
+
+    /**
+     * Adds a new movie to the database or modify the selected film.
+     * @param title
+     * @param genre
+     * @param director
+     * @param duration
+     * @param synopsis
+     * @param imagePath
+     * @param editType
+     * @throws SQLException
+     */
+    @Override
+    public void onValidateButtonClick(int movieID, String title, String genre, String director, String duration, String synopsis, String imagePath, String editType) throws SQLException {
+        try {
+            validateFields(title, genre, director, duration, synopsis, imagePath);
+            if (!imagePath.contains("AppData\\Roaming\\Applicine\\images\\")) {
+                imagePath = FileManager.copyImageToAppdata(imagePath);
+                System.out.println("Le chemin de l'image est " + imagePath);
+            }
+        } catch (InvalideFieldsExceptions e) {
+            showAlert(Alert.AlertType.ERROR, "Erreur", "Champs invalides", e.getMessage());
+            return;
+        }
+        if (editType.equals("add")) {
+            Movie newMovie = new Movie(title, genre, director, Integer.parseInt(duration), synopsis, createValidPath(imagePath));
+            movieDAO.addMovie(newMovie);
+        } else if (editType.equals("modify")) {
+            Movie existingMovie = createMovieWithRawData(movieID, title, genre, director, duration, synopsis, imagePath);
+            movieDAO.updateMovie(existingMovie);
+        }
+        System.out.println(imagePath);
+
+        movieList = fullFieldMovieListFromDB();
+        this.refreshMovieManager();
+    }
+
+
+    /**
+     *
+     * @param movieID
+     * @param title
+     * @param genre
+     * @param director
+     * @param duration
+     * @param synopsis
+     * We create a Movie object with data to use it to update database
+     * @return
+     */
+    private Movie createMovieWithRawData(int movieID, String title, String genre, String director, String duration, String synopsis, String imagePath) {
+        Movie existingMovie = movieDAO.getMovieById(movieID);
+        existingMovie.setTitle(title);
+        existingMovie.setGenre(genre);
+        existingMovie.setDirector(director);
+        existingMovie.setDuration(Integer.parseInt(duration));
+        existingMovie.setSynopsis(synopsis);
+        existingMovie.setImagePath(createValidPath(imagePath));
+        return existingMovie;
+    }
+
+
+
+    /**
+     * It opens a file chooser to choose an image.
+     */
+    @Override
+    public void onImageChoiceButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+        fileChooser.setTitle("Choisir une image");
+
+        String appdata = System.getenv("APPDATA");
+        String path = appdata + "/Applicine/images";
+
+        File initialDirectory = new File(path);
+        fileChooser.setInitialDirectory(initialDirectory);
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            String imagePath = selectedFile.getAbsolutePath();
+            System.out.println(imagePath);
+            movieManagerViewController.setImagePathLabel(imagePath);
+        }
+    }
+
+
+    /**
+     * It deletes a movie from the database.
+     * @param movieId
+     * @throws SQLException
+     */
+    public void onDeleteButtonClick(int movieId) throws SQLException {
+        try {
+            //Affiche une alerte de confirmation pour la suppression
+            boolean confirmed = showAlert(Alert.AlertType.CONFIRMATION, "Confirmation", "Suppression", "Voulez-vous vraiment supprimer ce film ?");
+            if (confirmed) {
+                movieDAO.removeMovie(movieId);
+                movieList = movieDAO.getAllMovies();
+                this.refreshMovieManager();
+                movieManagerViewController.deletionConfirmed();
+            }
+        } catch (SQLException e) {
+            showAlert(Alert.AlertType.ERROR, "Erreur", "Film introuvable", "Le film que vous essayez de supprimer n'existe pas");
+            return;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * It validates the fields of the movie by checking if they are empty or if the duration is a number.
+     * @param title
+     * @param genre
+     * @param director
+     * @param duration
+     * @param synopsis
+     * @param imagePath
+     * @throws InvalideFieldsExceptions
+     */
+
+    public void validateFields(String title, String genre, String director, String duration, String synopsis, String imagePath) throws InvalideFieldsExceptions {
+        if (title.isEmpty() || genre.isEmpty() || director.isEmpty() || duration.isEmpty() || synopsis.isEmpty() || imagePath.equals("...")) {
+            throw new InvalideFieldsExceptions("Tous les champs doivent être remplis");
+        }
+        try {
+            Integer.parseInt(duration);
+        } catch (NumberFormatException e) {
+            throw new InvalideFieldsExceptions("La durée doit être un nombre entier");
+        }
+    }
+
+
+    /**
+     * It returns the file name from the path by checking the operating system.
+     *
+     * @param path
+     * @return
+     */
+    public String getFileNameFrom(String path) {
+        System.out.println(System.getProperty("os.name") + " est le système d'exploitation actuel");
+
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            return path.substring(path.lastIndexOf("\\") + 1);
+        } else {
+            return path.substring(path.lastIndexOf("/") + 1);
+        }
+    }
+
+    /**
+     * It creates a valid path by checking if the path starts with "file:".
+     * This is necessary for the image to be displayed in the view.
+     * If the path does not start with "file:", it adds it.
+     *
+     * @param imagePath
+     * @return
+     */
+
+    public String createValidPath(String imagePath) {
+        if (imagePath.startsWith("file:")) {
+            return imagePath;
+        }
+        return "file:" + imagePath;
+    }
+
+    /**
+     * It refreshes the movie list by clearing the movies and adding them again.
+     */
+    public void refreshMovieManager() {
+        movieManagerViewController.clearMovies();
+        for (Movie movie : movieList) {
+            movieManagerViewController.displayMovie(movie);
+        }
+        movieManagerViewController.setSelection();
+        movieManagerViewController.refreshAfterEdit();
+    }
+    public void toLogin() throws IOException {
+        parentController.toLogin();
+    }
+
+
+}
---
+++ src/main/java/be/helha/applicine/controllers/managercontrollers/SessionManagerApp.java
@@ -0,0 +1,29 @@
+package be.helha.applicine.controllers.managercontrollers;
+
+import be.helha.applicine.views.managerviews.SessionManagerViewController;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class SessionManagerApp extends ManagerController {
+
+    private ManagerController parentController;
+
+    private FXMLLoader sessionManagerFxmlLoader;
+
+    private SessionManagerViewController sessionManagerViewController;
+
+    public SessionManagerApp() {
+        super();
+    }
+
+    @Override
+    public void start(Stage adminPage) throws Exception {
+        sessionManagerFxmlLoader = parentController.getSessionManagerFXML();
+        sessionManagerViewController = sessionManagerFxmlLoader.getController();
+    }
+
+
+    public void setParentController(ManagerController managerController) {
+        this.parentController = managerController;
+    }
+}
