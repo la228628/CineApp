47130e1793ac19af7a64403e4e26bbf984598ae2
Lucas Vanderzielen <la229879@student.helha.be>
2024-04-11 12:35:22 +0200

    Ticket Price bugged

--- src/main/java/be/helha/applicine/views/TicketShoppingViewController.java
+++ src/main/java/be/helha/applicine/views/TicketShoppingViewController.java
@@ -1,14 +1,21 @@
 package be.helha.applicine.views;
 
-import be.helha.applicine.models.Ticket;
+import eu.hansolo.tilesfx.TestLauncher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
 
 public class TicketShoppingViewController {
     @FXML
@@ -18,15 +25,46 @@ public class TicketShoppingViewController {
     @FXML
     public TextField normalPlaceNumber;
 
+    private List<Double> ticketPrices =  new ArrayList<>();
     private TextField getButtonTextField(ActionEvent actionEvent){
         errorBox.setVisible(false);
         Node sourceNode = (Node) actionEvent.getSource();
         HBox container = (HBox) sourceNode.getParent();
-        return (TextField) container.getChildren().stream()
+        TextField textField = (TextField) container.getChildren().stream()
                 .filter(node -> node instanceof TextField)
                 .findFirst()
                 .orElse(null);
+        updatePrice((VBox) container.getParent());
+        return textField;
     }
+
+    public List<TextField> getTextFields(HBox menuButton) {
+        return menuButton.getChildren().stream()
+                .filter(node -> node instanceof TextField)
+                .map(node -> (TextField) node)
+                .toList();
+    }
+
+    public ArrayList<Integer> getTicketsNumber(){
+
+    }
+
+    public void updatePrice(VBox menuButton){
+        System.out.println("updatePrice");
+        for (int i = 0; i<menuButton.getChildren().size(); i++){
+            HBox hBox = (HBox) menuButton.getChildren().get(i);
+            List<TextField> textFields = getTextFields(hBox);
+            for (TextField textField : textFields){
+                try {
+                    int currentTicketNumber = Integer.parseInt(textField.getText());
+                    ticketPrices.set(i, (currentTicketNumber * 8.5));
+                }catch (Exception e){
+                    errorBox.setVisible(true);
+                }
+            }
+        }
+    }
+
     public void addTicket(ActionEvent actionEvent) {
         TextField buttonTextField = getButtonTextField(actionEvent);
         try {
@@ -36,6 +74,7 @@ public class TicketShoppingViewController {
             errorBox.setVisible(true);
         }
     }
+
     public void removeTicket(ActionEvent actionEvent){
         TextField buttonTextField = getButtonTextField(actionEvent);
         try {
