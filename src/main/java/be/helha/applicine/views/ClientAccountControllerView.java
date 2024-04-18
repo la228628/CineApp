c19a4542770006eaab4feeb52f6de7f1b3b5c9cc
Lucas Vanderzielen <la229879@student.helha.be>
2024-04-18 20:26:41 +0200

    Exceptions

--- src/main/java/be/helha/applicine/views/ClientAccountControllerView.java
+++ src/main/java/be/helha/applicine/views/ClientAccountControllerView.java
@@ -52,7 +52,6 @@ public class ClientAccountControllerView {
             accountWindow.setScene(scene); //définit la scène de la fenêtre
             accountWindow.setTitle("Client Account"); //définit le titre de la fenêtre
             accountWindow.show();
-            throw new Exception();
     }
     public void onCloseButtonClicked(ActionEvent actionEvent) throws Exception {
         //TO DO j'informe le client que ses modifications ne seront pas enregistrées
