7e23ae98ca01d429cd5f7fdc5479973ccb35cf59
Lucas Vanderzielen <la229879@student.helha.be>
2024-05-24 22:06:17 +0200

    Javadoc

--- src/main/java/be/helha/applicine/common/models/request/DeleteViewableRequest.java
+++ src/main/java/be/helha/applicine/common/models/request/DeleteViewableRequest.java
@@ -9,31 +9,59 @@ public class DeleteViewableRequest extends ClientEvent {
 
     private String message;
 
+    /**
+     * Constructor of the request
+     * @param viewableId the id of the viewable to delete
+     */
     public DeleteViewableRequest(int viewableId) {
         this.viewableId = viewableId;
     }
 
+    /**
+     * Getter of the id for the viewable
+     * @return the id of the viewable
+     */
     public int getViewableId() {
         return viewableId;
     }
 
+    /**
+     * Dispatch the request to the visitor to handle it.
+     * @param requestVisitor the visitor
+     */
     @Override
     public void dispatchOn(RequestVisitor requestVisitor) {
         requestVisitor.visit(this);
     }
 
+    /**
+     * Setter of the success of the request
+     * @param b the success of the request
+     */
     public void setSuccess(boolean b) {
         this.success = b;
     }
 
+    /**
+     * Getter of the success of the request
+     * @return the success of the request
+     */
     public boolean getSuccess() {
         return success;
     }
 
+    /**
+     * Setter of the message
+     * @param message the message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Getter of the message
+     * @return the message
+     */
     public String getMessage() {
         return message;
     }
